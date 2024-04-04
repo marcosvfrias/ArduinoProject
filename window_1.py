a3b7aea0590d385aa137dcabf92a4f9d31e95976
febeee-mt <127672893+febeee-mt@users.noreply.github.com>
2024-04-03 21:55:27 -0400

    Update window_1.py

--- window_1.py
+++ window_1.py
@@ -1,23 +1,12 @@
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk
+from tkinter import BooleanVar, IntVar, ttk
 
-style = ttk.Style()
 
 def close():
     root.destroy()
 
-def change_color(boton):
-    style.configure('Custom.TButton', background='#50bbf0')
-    boton.configure(style='Custom.TButton')
-
-def input_color(input):
-    style.configure('Custom.TEntry', background='#a4a4a4')
-    input.configure(style='Custom.TEntry')
-
 root = tk.Tk()
 root.title("Monitoring")
-root.iconphoto(True, PhotoImage(file="python-logo.png"))
 root.geometry("750x500")
 root.resizable(0, 0)
 
@@ -28,7 +17,6 @@ button_frame.pack(pady=15)
 #buttons
 btn_mntr = ttk.Button(button_frame, text="Monitoring", width=15)
 btn_mntr.pack(side=tk.LEFT)
-root.bind("<Map>", lambda event: change_color(btn_mntr))
 btn_control = ttk.Button(button_frame, text="Manual Control", width=20, command=close)
 btn_control.pack(side=tk.LEFT)
 
@@ -50,21 +38,21 @@ temp_frame = ttk.Frame(inputs_frame, relief="groove")
 temp_frame.pack(side=tk.LEFT, pady=30, padx=10)
 input1 = ttk.Entry(temp_frame, textvariable=IntVar())
 input1.insert(0,"Input 1 - Temperature")
-input1.bind("<Button-1>", lambda e: input1.delete(0, tk.END), input_color(input1))
+input1.bind("<Button-1>", lambda e: input1.delete(0, tk.END))
 input1.pack(padx=10, pady=10, ipadx=20, ipady=5)
 
 hum_frame = ttk.Frame(inputs_frame, relief="groove")
 hum_frame.pack(side=tk.LEFT, pady=30, padx=10)
 input2 = ttk.Entry(hum_frame, textvariable=IntVar())
 input2.insert(0,"Input 2 - Humidity")
-input2.bind("<Button-1>", lambda e: input2.delete(0, tk.END), input_color(input2))
+input2.bind("<Button-1>", lambda e: input2.delete(0, tk.END))
 input2.pack(padx=10, pady=10, ipadx=20, ipady=5)
 
 bool_frame = ttk.Frame(inputs_frame, relief="groove")
 bool_frame.pack(side=tk.LEFT, pady=30, padx=10)
 input3 = ttk.Entry(bool_frame, textvariable=BooleanVar())
 input3.insert(0,"Input 3 - Boolean")
-input3.bind("<Button-1>", lambda e: input3.delete(0, tk.END), input_color(input3))
+input3.bind("<Button-1>", lambda e: input3.delete(0, tk.END))
 input3.pack(padx=10, pady=10, ipadx=20, ipady=5)
 
 #separator line
@@ -82,21 +70,21 @@ label_out.pack(side=tk.LEFT, anchor=tk.N, padx=10, pady=25)
 out1_frame = ttk.Frame(outputs_frame, relief="groove")
 out1_frame.pack(side=tk.LEFT, pady=50, padx=10)
 out1 = ttk.Label(out1_frame, text="Output 1", relief="groove")
-out1.pack(padx=10, pady=10, ipady=5, ipadx=30)
+out1.pack(padx=10, pady=10, ipady=5, ipadx=10)
 
 out2_frame = ttk.Frame(outputs_frame, relief="groove")
 out2_frame.pack(side=tk.LEFT, pady=50, padx=10)
 out2 = ttk.Label(out2_frame, text="Output 2", relief="groove")
-out2.pack(padx=10, pady=10, ipady=5, ipadx=30)
+out2.pack(padx=10, pady=10, ipady=5, ipadx=10)
 
 out3_frame = ttk.Frame(outputs_frame, relief="groove")
 out3_frame.pack(side=tk.LEFT, pady=50, padx=10)
 out3 = ttk.Label(out3_frame, text="Output 3", relief="groove")
-out3.pack(padx=10, pady=10, ipady=5, ipadx=30)
+out3.pack(padx=10, pady=10, ipady=5, ipadx=10)
 
 out4_frame = ttk.Frame(outputs_frame, relief="groove")
 out4_frame.pack(side=tk.LEFT, pady=50, padx=10)
 out4 = ttk.Label(out4_frame, text="Output 4", relief="groove")
-out4.pack(padx=10, pady=10, ipady=5, ipadx=30)
+out4.pack(padx=10, pady=10, ipady=5, ipadx=10)
 
 root.mainloop()
